b72c66f7f97678edb82b94df70fe78eeec8203bd
Oscar99 <od_cortez@galileo.edu>
2015-01-22 12:04:37 -0600

    Get angular

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// Javascript Code.
+angular.module("App", [])
+ .controller("Controller", function($scope, $resource) {
+
+  $resource.get("data/2015-1-1-15.json").success(function(data, status, headers, config){
+	$scope.playlists = data;
+	console.log(data);
+  });
+ });
