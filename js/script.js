c880de4ce99f654965209eb38dbaad7a50df6f71
brandon <josueknd@hotmail.es>
2015-01-23 12:01:23 -0600

    Last fm api

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-angular.module("App", [])
- .controller("Controller", function($scope, $resource) {
+	var app = angular.module("App", [])
 
-  $resource.get("data/2015-1-1-15.json").success(function(data, status, headers, config){
-	$scope.playlists = data;
-	console.log(data);
-  });
- });
+	app.controller("Controller", function($scope, $http, $filter) {
+		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=05d4b311160508e4320ec650346e45c8&format=json").success(function(data){
+		var orderBy = $filter("orderBy");
+		$scope.playlists = data.tracks.track;
+		$scope.order = function(predicate, reverse) {
+			$scope.playlists = orderBy($scope.playlists, predicate, reverse);
+
+			};
+		});
+
+	});
+
