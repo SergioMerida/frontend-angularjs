961399d50ce00b2dd0b70d22d1adf52c3af5ea0a
Sergio Merida <sergiotabarini@hotmail.com>
2015-01-24 12:15:54 -0600

    Search and add of images

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-	var app = angular.module("App", [])
+	
+	var lastFm = angular.module("lastFm", [])
 
-	app.controller("Controller", function($scope, $http, $filter) {
+	lastFm.filter("soloUrl", function(){
+		return function(item){
+			return (JSON.stringify(item)).slice(10,-18);
+		};
+	});
+
+	lastFm.controller("getData", function($scope, $http, $filter) {
 		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=05d4b311160508e4320ec650346e45c8&format=json").success(function(data){
 		var orderBy = $filter("orderBy");
 		$scope.playlists = data.tracks.track;
-		console.log($scope.playlists)
+		
+		/*Order by*/
 		$scope.order = function(predicate, reverse) {
 			$scope.playlists = orderBy($scope.playlists, predicate, reverse);
 
