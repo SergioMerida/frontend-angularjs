a3f45e6cf9a4a04a5dc3be7024e0ebe3dd160784
Sergio Tabarini <tabarinisergio@gmail.com>
2015-01-26 09:40:30 -0600

    Adding panel and table responsive

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-	
+	/*Module lastFm*/
 	var lastFm = angular.module("lastFm", [])
 
+	/*Filter to get the url of the images*/
 	lastFm.filter("soloUrl", function(){
 		return function(item){
 			return (JSON.stringify(item)).slice(10,-18);
 		};
 	});
 
+	/*Get the data from the api of lastFm*/
 	lastFm.controller("getData", function($scope, $http, $filter) {
 		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=05d4b311160508e4320ec650346e45c8&format=json").success(function(data){
 		var orderBy = $filter("orderBy");
@@ -15,7 +17,6 @@
 		/*Order by*/
 		$scope.order = function(predicate, reverse) {
 			$scope.playlists = orderBy($scope.playlists, predicate, reverse);
-
 			};
 		});
 
